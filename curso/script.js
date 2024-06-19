3e58f022ef2867ea62a540ebba8ae05cdc748239
Moisés <146381238+pardim06@users.noreply.github.com>
2024-06-19 13:20:20 -0300

    Add files via upload
    ativ 3

--- curso/script.js
+++ curso/script.js
@@ -1,78 +1,17 @@
 
-var palavras = ["ARROZ", "FEIJAO", "MACARRAO", "BATATA", "CENOURA", "ABACAXI", "MELANCIA", "BANANA", "MANGA", "LARANJA"];
-
-var palavraSecreta = palavras[Math.floor(Math.random() * palavras.length)];
-var letrasUtilizadas = [];
-var numTentativas = 6;
-var letrasCertas = 0;
-
-
-var palavraEscondida = [];
-for (var i = 0; i < palavraSecreta.length; i++) {
-    palavraEscondida.push("_");
+function girarD6() {
+    var resultado = Math.floor(Math.random() * 6) + 1; 
+    document.getElementById('resultadoD6').innerText = "Resultado: " + resultado;
 }
 
 
-document.getElementById('palavra').innerText = palavraEscondida.join(' ');
-
-
-function checarLetra() {
-    var letra = document.getElementById('letra').value.toUpperCase();
-
-    
-    if (!/^[A-Z]$/.test(letra)) {
-        exibirMensagem("Digite apenas uma letra válida.");
-        return;
-    }
-
-   
-    if (letrasUtilizadas.includes(letra)) {
-        exibirMensagem("Esta letra já foi utilizada.");
-        return;
-    }
-
-    letrasUtilizadas.push(letra);
-    document.getElementById('letrasUsadas').innerText = letrasUtilizadas.join(', ');
-
-    var letraEncontrada = false;
-    for (var i = 0; i < palavraSecreta.length; i++) {
-        if (palavraSecreta[i] === letra) {
-            palavraEscondida[i] = letra;
-            letraEncontrada = true;
-            letrasCertas++;
-        }
-    }
-
-   
-    document.getElementById('palavra').innerText = palavraEscondida.join(' ');
-
-   
-    if (letrasCertas === palavraSecreta.length) {
-        exibirMensagem("voce ganhou  deu sorte A palavra era: " + palavraSecreta);
-        desabilitarEntrada();
-        return;
-    }
-
-    if (!letraEncontrada) {
-        numTentativas--;
-        document.getElementById('numTentativas').innerText = numTentativas;
-    }
-
-    if (numTentativas === 0) {
-        exibirMensagem("errou kkkkk A palavra era: " + palavraSecreta);
-        desabilitarEntrada();
-    }
-
-    
-    document.getElementById('letra').value = '';
+function girarD8() {
+    var resultado = Math.floor(Math.random() * 8) + 1; 
+    document.getElementById('resultadoD8').innerText = "Resultado: " + resultado;
 }
 
 
-function exibirMensagem(mensagem) {
-    document.getElementById('mensagem').innerText = mensagem;
-}
-
-
-function desabilitarEntrada() {
-    document.getElementById('entrada').style.display = 'none';
+function girarD20() {
+    var resultado = Math.floor(Math.random() * 20) + 1; 
+    document.getElementById('resultadoD20').innerText = "Resultado: " + resultado;
 }
